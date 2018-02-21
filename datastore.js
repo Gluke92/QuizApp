1b2f58401c97d644bfd88eb2944f32960ba0c4c0
George Luke <george.luke92@gmail.com>
2018-02-21 16:00:57 -0600

    added class to HTML, added first event listener to fire on js-next-question

---
+++ datastore.js
@@ -0,0 +1,115 @@
+'use strict';
+
+const STORE = [
+  { 
+      /* Q1 */ 
+    question: 'What is the fundamental condition that causes an economy to exist?' , 
+    answers: [
+      'Scarcity',
+      'The gold standard',
+      'Resources',
+      'Class warfare'
+    ],
+    correctAnswer: 'Scarcity'
+    /* display error message */
+  },
+  {
+      /* Q2 */
+    question: 'When do we experience economic scarcity?',
+    answers: [
+      'When a given amount of goods has limited and mutually exclusive alternative uses',
+      'When goods are hard to find due to the depeltion of natural resources.',
+      'When a particular good is a rare find a given context',
+      'When people really want new Jordans'
+    ],
+    correctAnswer: 'When a given amount of goods has limited and mutually exclusive alternative uses',
+  },
+  {
+      /* Q3 */
+    question: 'One of the most effective methods for making housing affordable is to set a price ',
+    answers: [
+      'Yes. It\'ll make all the girls go wo.',
+      'Yes. If you\'re short.',
+      'No. It makes your landlord angry.',
+      'No. Remember Ozwald? Right. That\'s the point.'
+    ],
+      correctAnswer: 'Yes. If you\'re short.'
+  },
+  {
+      /* Q4 */
+      question: ' In the 16th century, the Spanish colony Antwerp rebelled against Spain. Spain sent a blockade to starve Antwerp out and force their surrender. As the supply of goods from Spain ceased, what did this do to the price of goods in Antwerp?',
+    answers: [
+      'Nothing.',
+      'The price increased, because the law of supply tells us that when consumers have less supplies, they’ll pay more money.',
+      'The price increased, because the law of supply tells us that producers will charge greater prices when they have fewer supplies.',
+      'The price increased, because the level of demand from Antwerp stayed the same, but the level of goods supplied by Spain decreased. Therefore, the price of goods in Spain increased.'
+    ],
+      correctAnswer: 'The price increased, because the level of demand from Antwerp stayed the same, but the level of goods supplied by Spain decreased. Therefore, the price of goods in Spain increased.'
+  },
+  {
+      /* Q5 */
+    question: '',
+    answers: [
+      '',
+      '',
+      '',
+      ''
+    ],
+    correctAnswers: 
+  },
+  {
+      /* Q6 */
+    question: '',
+    answers: [
+      '',
+      '',
+      '',
+      ''
+    ],
+    correctAnswers: 
+  },
+  {
+      /* Q7 */
+    question: '',
+    answers: [
+      '',
+      '',
+      '',
+      ''
+    ],
+    correctAnswers: 
+  },
+  {
+      /* Q8 */
+    question: '',
+    answers: [
+      '',
+      '',
+      '',
+      ''
+    ],
+    correctAnswers: 
+  },
+  { 
+      /* Q9 */ 
+    question: '',
+    answers: [
+      '',
+      '',
+      '',
+      ''
+    ],
+    correctAnswers: 
+  },
+  {
+      /* Q10 */ 
+    question: '',
+    answers: [
+      '',
+      '',
+      '',
+      ''
+    ],
+    correctAnswers: 
+  }
+];
---
+++ main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let questionNumber = 0;
+let score = 0;
+
+/* navigate from splash page to quiz */
+$('js-next-question').on('click', event => {
+    console.log('Wutup');
+});
+
+
+// if (questionNumber < STORE.legnth) {
+
+// }
