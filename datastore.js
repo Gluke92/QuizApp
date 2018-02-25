82c65e283fb4e111239a270a95a49a218c66f98d
George Luke <george.luke92@gmail.com>
2018-02-24 22:49:26 -0600

    working on storing submit value, confused about capuring value

--- datastore.js
+++ datastore.js
@@ -1,9 +1,9 @@
 'use strict';
-questionNumber = 0;
-
+// let currentQuestion = 0;
+// Object.keys(STORE[0].answers).map(id => {STORE[currentQuestion].answers[id]}) -> 1st ID -> returns array of [0, 1, 2, 3] 
 const STORE = [
   { 
-      /* Q1 */ 
+    /* Q1 */ 
     question: 'What is the fundamental condition that causes an economy to exist?' , 
     answers: [
       'Scarcity',
@@ -11,11 +11,13 @@ const STORE = [
       'Resources',
       'Class warfare'
     ],
-    correctAnswer: 'Scarcity'
+    correctAnswer: 0 /* index of correct answer */ 
+    // answers[correctAnswer] to get correct answer. 
+    //randomizing answers -> answers into Object -> use Object.keys
     /* display error message */
   },
   {
-      /* Q2 */
+    /* Q2 */
     question: 'When do we experience economic scarcity?',
     answers: [
       'When a given amount of goods has limited and mutually exclusive alternative uses',
@@ -26,7 +28,7 @@ const STORE = [
     correctAnswer: 'When a given amount of goods has limited and mutually exclusive alternative uses',
   },
   {
-      /* Q3 */
+    /* Q3 */
     question: 'One of the most effective methods for making housing affordable is to set a price ',
     answers: [
       'Yes. It\'ll make all the girls go wo.',
@@ -34,83 +36,83 @@ const STORE = [
       'No. It makes your landlord angry.',
       'No. Remember Ozwald? Right. That\'s the point.'
     ],
-      correctAnswer: 'Yes. If you\'re short.'
+    correctAnswer: 'Yes. If you\'re short.'
   },
   {
-      /* Q4 */
-      question: ' In the 16th century, the Spanish colony Antwerp rebelled against Spain. Spain sent a blockade to starve Antwerp out and force their surrender. As the supply of goods from Spain ceased, what did this do to the price of goods in Antwerp?',
+    /* Q4 */
+    question: ' In the 16th century, the Spanish colony Antwerp rebelled against Spain. Spain sent a blockade to starve Antwerp out and force their surrender. As the supply of goods from Spain ceased, what did this do to the price of goods in Antwerp?',
     answers: [
       'Nothing.',
       'The price increased, because the law of supply tells us that when consumers have less supplies, they’ll pay more money.',
       'The price increased, because the law of supply tells us that producers will charge greater prices when they have fewer supplies.',
       'The price increased, because the level of demand from Antwerp stayed the same, but the level of goods supplied by Spain decreased. Therefore, the price of goods in Spain increased.'
     ],
-      correctAnswer: 'The price increased, because the level of demand from Antwerp stayed the same, but the level of goods supplied by Spain decreased. Therefore, the price of goods in Spain increased.'
+    correctAnswer: 'The price increased, because the level of demand from Antwerp stayed the same, but the level of goods supplied by Spain decreased. Therefore, the price of goods in Spain increased.'
   },
   {
-      /* Q5 */
-    question: '',
+    /* Q5 */
+    question: 'In India, in the eighteenth century, there were famines in Bagawat. In order to combat the reality of starving people, the Indian government subsidized (supported) the growing of grain. This meant that farmers were being supported to produce a certain quota of grain, regardless of the market demand. Relative to scarcity, how was this a myopic response to the famine? Sure why not',
     answers: [
-      '',
-      '',
-      '',
-      ''
+      'Sure why not',
+      'I know you are but what am I',
+      'Huh',
+      'Nope'
     ],
-    correctAnswers: 
+    correctAnswers: 'Sure why not'
   },
   {
-      /* Q6 */
-    question: '',
+    /* Q6 */
+    question: 'In the 19th century, these famines occurred again, but for various reasons, the British government which had made India a colony chose to have a free market for trade to occur. What do you predict happened and why?',
     answers: [
-      '',
-      '',
-      '',
-      ''
+      'NO change.',
+      'Famine still went.',
+      'Famine was met',
+      'Famine was met. '
     ],
-    correctAnswers: 
+    correctAnswers: 'Famine was met'
   },
   {
-      /* Q7 */
-    question: '',
+    /* Q7 */
+    question: ' Is knowledge a scarce good?',
     answers: [
-      '',
-      '',
-      '',
-      ''
+      'No',
+      'Y',
+      'N',
+      'Y'
     ],
-    correctAnswers: 
+    correctAnswers: 'Y'
   },
   {
-      /* Q8 */
-    question: '',
+    /* Q8 */
+    question: 'How is a material shortages different than an artificial shortage?',
     answers: [
-      '',
-      '',
-      '',
-      ''
+      'N',
+      'N',
+      'Y',
+      'N'
     ],
-    correctAnswers: 
+    correctAnswers: 'Y'
   },
   { 
-      /* Q9 */ 
-    question: '',
+    /* Q9 */ 
+    question: 'What does a “price” represent? ',
     answers: [
-      '',
-      '',
-      '',
-      ''
+      'N',
+      'N',
+      'N',
+      'Y'
     ],
-    correctAnswers: 
+    correctAnswers: 'Y'
   },
   {
-      /* Q10 */ 
-    question: '',
+    /* Q10 */ 
+    question: 'In the Soviet Union, there was a centralized and planned economy. Theoretically, the more control we can exert over production and supply, the more efficiently we can meet the needs of those who request given goods and resist the corruption of those who would demand a high price. ',
     answers: [
-      '',
-      '',
-      '',
-      ''
+      'N',
+      'Y',
+      'N',
+      'N'
     ],
-    correctAnswers: 
+    correctAnswers: 'Y'
   }
 ];
--- main.js
+++ main.js
@@ -1,14 +1,105 @@
 'use strict';
+/* global STORE */
 
-let questionNumber = 0;
 let score = 0;
+let currentQuestion = 0;
+
+
+
+/* when user clicks on next button, add one to current question */
+$('.js-generate-form').on('click', '.submit-button', event => {
+  currentQuestion++;
+  score =+ 10;
+});
+
+function generateQuestion() {
+  if (currentQuestion < STORE.length) {
+    return `<h2>${STORE[currentQuestion].question}</h2>
+<div class="js-generate-form">
+    <form>
+        <fieldset>
+            <label class="">
+                <input type="radio" value="${STORE[currentQuestion].answers[0]}" name="answer" required>
+                <span>${STORE[currentQuestion].answers[0]}</span>
+            </label>
+            <label class="">
+                <input type="radio" value="${STORE[currentQuestion].answers[1]}" name="answer" required>
+                <span>${STORE[currentQuestion].answers[1]}</span>
+            </label>
+            <label class="">
+                <input type="radio" value="${STORE[currentQuestion].answers[2]}" name="answer" required>
+                <span>${STORE[currentQuestion].answers[2]}</span>
+            </label>
+            <label class="">
+                <input type="radio" value="${STORE[currentQuestion].answers[3]}" name="answer" required>
+                <span>${STORE[currentQuestion].answers[3]}</span>
+            </label>
+        </fieldset>
+    </form>
+    <button type="submit" class="submit-button">Submit</button>
+</div>`;
+  } 
+  else {
+    `<div>
+    <h1>A Primer on Basic Economics</h1>
+    <!-- If you know >8 -->
+    <p>Newsweek estimates that over 48% of Americans have no idea how the economy works. Now you're in the 52% who get the basics.</p>
+    <!-- If you know between 4-7 -->
+    <p>As Arisottle said, "Excellent is not an act, but a habit." Keep working.</p>
+    <!-- If you know less less than 4 -->
+    <p>Newsweek estimates that over 48% of Americans have no idea how the economy works. You're still there.
+    </p>
+
+    <button type="submit" class="">Let's Learn How Money Works</button>
+    <button type="submit" class="">Retake the Quiz</button>
+</div>`;
+  }
+}
+
 
 /* navigate from splash page to quiz */
 $('.js-start').on('click', '.start-button', event => {
-  console.log('Wutup');
+  $('.js-start').addClass('hidden');
+});
+
+/*genearte HTML for quiz */
+$('.js-start').on('click', '.start-button', event => {
+  $('.js-form').css('display', 'block');
+  $('.js-form').html(generateQuestion());
+  console.log('did it');
+});
+
+// /*when you click button, we should modularize here*/
+// $('.js-start').on('click', '.start-button', event => {
+//   $('.js-form').html(generateQuestion());
+// });
+
+
+// /*add 1 when you hit submit button*/
+$('.js-main').on('click', '.submit-button', event => {
+  event.preventDefault();
+  console.log('submitting like a boss');
+
+  // //augment current Question
+  currentQuestion++;
+
+  // regenerate the form 
+  $('.js-form').html(generateQuestion());
+
+  //store value from submit
+  const isItRight = $('input[type=radio][name=answer]:checked').val();
+  console.log(isItRight);
+
+  //compare value to key
+
+  //change score based on value
+
+  //update value
+
+  //print new text for value
+
+  //render next question
 });
 
 
-// if (questionNumber < STORE.legnth) {
 
-// }
